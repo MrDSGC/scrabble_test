69e2043d36c21a33510b0703d24a6341a769c417
MrDSGC <dcheng47@gmail.com>
2017-09-17 17:43:03 -0700

    components

---
+++ frontend/components/main_page.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render () {
+    return(
+      <div className='full-page'>
+        <div className='content'>
+        </div>
+      </div>
+
+    )
+  };
+}
+
+export default MainPage;
--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
+import Mainpage from './main_page'
 
 const Root = ({ store }) => {
 
   return(
     <Provider store={ store }>
       <Router history={ hashHistory }>
-        <Route path="/" component={}>
+        <Route path="/" component={ MainPage }>
 
         </Route>
       </Router>
