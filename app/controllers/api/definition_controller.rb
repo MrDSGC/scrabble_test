3ffab21e47b31a53f3aa052bdc5810c508e254d4
MrDSGC <dcheng47@gmail.com>
2017-09-17 17:35:25 -0700

    frontend

--- app/controllers/api/definition_controller.rb
+++ app/controllers/api/definition_controller.rb
@@ -1,10 +1,11 @@
 class Api::DefinitionController < ApplicationController
 
   def create
+    @definition = Definition.new(definition_params)
   end
 
   def definition_params
-    params.require(:definition).permit(:definition)
+    params.require(:definition).permit(:definition, :word_id)
   end
 
 end
